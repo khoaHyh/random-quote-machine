84c3b71a5de2cac0ef1ce2138c10f0bedbaad05a
khoaHyh <khoahuynh.toronto@gmail.com>
2020-12-14 20:16:06 -0500

    Style: Add basic styling
    Add basic styling to visualize layout and assist in development

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,18 +1,30 @@
-import './App.css';
-import ReactFCCtest from 'react-fcctest';
+import React, { Component } from "react";
+import "./App.css";
+import ReactFCCtest from "react-fcctest";
 
-function App() {
-  return (
-    <div className="App">
-      <ReactFCCtest />
-      <div id="quote-box">
-        <div id="text"></div>
-        <div id="author"></div>
-        <div id="new-quote"></div>
-        <a id="tweet-quote" href="twitter.com/intent/tweet" target="_blank" rel="noreferrer noopener"> </a>
+
+
+class App extends Component {
+
+  // async componentDidMount() {
+  //   const url = "https://type.fit/api/quotes"
+  // }
+
+  render() {
+    return (
+      <div className="App">
+        <ReactFCCtest />
+        <div id="quote-box">
+          <div id="text">Random Quote goes here</div>
+          <div id="author">Respective Author</div>
+          <div className="buttons">
+            <a id="tweet-quote" href="twitter.com/intent/tweet" target="_blank" rel="noreferrer noopener">Tweet Quote button</a>
+            <div id="new-quote">New Quote button</div>
+          </div>
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
