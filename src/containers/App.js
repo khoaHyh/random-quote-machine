2fba2eea483126efbea45390f447c35188fc2418
khoaHyh <khoahuynh.toronto@gmail.com>
2020-12-14 19:35:15 -0500

    Feat: Add user story divs
    Initialize divs to fulfill user stories on FCC

--- src/containers/App.js
+++ src/containers/App.js
@@ -5,8 +5,12 @@ function App() {
   return (
     <div className="App">
       <ReactFCCtest />
-      <header className="App-header">
-      </header>
+      <div id="quote-box">
+        <div id="text"></div>
+        <div id="author"></div>
+        <div id="new-quote"></div>
+        <a id="tweet-quote" href="twitter.com/intent/tweet" target="_blank" rel="noreferrer noopener"> </a>
+      </div>
     </div>
   );
 }
