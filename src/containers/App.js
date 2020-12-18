9dcb0958f716165d728dbbd5499251af23eb4bcc
khoaHyh <khoahuynh.toronto@gmail.com>
2020-12-18 11:18:49 -0500

    Refactor: Update to use react hooks
    Update app.js to use react hooks

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,53 +1,91 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 import ReactFCCtest from "react-fcctest";
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      quote: '',
-      author: '',
-      newQuote: false
-    }
-    this.generateRandomQuote = this.generateRandomQuote.bind(this);
-  }
+const App = () => {
+  const quoteVariable = useState('');
+  let quote = quoteVariable[0];
+  let setQuote = quoteVariable[1];
 
-  async componentDidMount() {
-    this.generateRandomQuote();
-  }
+  const authorVariable = useState('');
+  let author = authorVariable[0];
+  let setAuthor = authorVariable[1];
 
-  generateRandomQuote = async() => {
+  useEffect(() => generateRandomQuote(), []);
+
+  const generateRandomQuote = async() => {
     try {
       const url = "https://type.fit/api/quotes";
       const response = await fetch(url);
       const data = await response.json();
       let randomIndex = Math.round(Math.random() * (data.length - 1));
-      this.setState({
-        quote: data[randomIndex].text,
-        author: data[randomIndex].author
-      });
+      setQuote(data[randomIndex].text);
+      setAuthor(data[randomIndex].author);
     } catch(err) {
       console.log(`error: ${err}`);
     }
   }
 
-  render() {
-    const { quote, author } = this.state;
-    return (
-      <div className="App">
-        <ReactFCCtest />
-        <div id="quote-box">
-          <div id="text">{quote}</div>
-          <div id="author">{author}</div>
-          <div className="buttons">
-            <a id="tweet-quote" href="https://twitter.com/intent/tweet" target="_blank" rel="noreferrer noopener">Tweet Quote button</a>
-            <button id="new-quote" onClick={this.generateRandomQuote}>New Quote</button>
-          </div>
+  return (
+    <div className="App">
+      <ReactFCCtest />
+      <div id="quote-box">
+        <div id="text">{quote}</div>
+        <div id="author">{author}</div>
+        <div className="buttons">
+          <a id="tweet-quote" href="https://twitter.com/intent/tweet" target="_blank" rel="noreferrer noopener">Tweet Quote button</a>
+          <button id="new-quote" onClick={generateRandomQuote}>New Quote</button>
         </div>
       </div>
-    );
-  }
+    </div>
+  );
 }
+
+// class App extends Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       quote: '',
+//       author: ''
+//     }
+//     this.generateRandomQuote = this.generateRandomQuote.bind(this);
+//   }
+
+//   async componentDidMount() {
+//     this.generateRandomQuote();
+//   }
+
+//   generateRandomQuote = async() => {
+//     try {
+//       const url = "https://type.fit/api/quotes";
+//       const response = await fetch(url);
+//       const data = await response.json();
+//       let randomIndex = Math.round(Math.random() * (data.length - 1));
+//       this.setState({
+//         quote: data[randomIndex].text,
+//         author: data[randomIndex].author
+//       });
+//     } catch(err) {
+//       console.log(`error: ${err}`);
+//     }
+//   }
+
+//   render() {
+//     const { quote, author } = this.state;
+//     return (
+//       <div className="App">
+//         <ReactFCCtest />
+//         <div id="quote-box">
+//           <div id="text">{quote}</div>
+//           <div id="author">{author}</div>
+//           <div className="buttons">
+//             <a id="tweet-quote" href="https://twitter.com/intent/tweet" target="_blank" rel="noreferrer noopener">Tweet Quote button</a>
+//             <button id="new-quote" onClick={this.generateRandomQuote}>New Quote</button>
+//           </div>
+//         </div>
+//       </div>
+//     );
+//   }
+// }
 
 export default App;
